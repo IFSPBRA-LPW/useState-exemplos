13e0d5010f487488d1a59a4d033fb2b7128dcbe6
ana <martins.ana@ifsp.edu.br>
2025-08-15 17:04:35 -0300

    correçao exemplo 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import { useState } from "react";
 
 // Exemplo 1:
-// export default function Contador() {
-//   const [contador, setContador] = useState(0);
+export default function Contador() {
+  const [contador, setContador] = useState(0);
 
-//   function aumentar() {
-//     setContador(contador + 1);
-//   }
+  function aumentar() {
+    setContador(contador + 1);
+  }
 
-//   function reduzir() {
-//     setContador(contador + 1);
-//   }
+  function reduzir() {
+    setContador(contador - 1);
+  }
 
-//   return (
-//     <div>
-//       <h1>Valor: {contador}</h1>
-//       <button onClick={aumentar}>+</button>
-//       <button onClick={reduzir}>-</button>
-//     </div>
-//   );
+  return (
+    <div>
+      <h1>Valor: {contador}</h1>
+      <button onClick={aumentar}>+</button>
+      <button onClick={reduzir}>-</button>
+    </div>
+  );
 
 // Exemplo 2:
 // export default function NomeUsuario() {
@@ -36,25 +36,25 @@ import { useState } from "react";
 //   );
 
 // Exemplo 3
-export default function Perfil() {
-  const [usuario, setUsuario] = useState({
-    nome: "João",
-    idade: 25,
-  });
+// export default function Perfil() {
+//   const [usuario, setUsuario] = useState({
+//     nome: "João",
+//     idade: 25,
+//   });
 
-  function fazerAniversario() {
-    setUsuario((prev) => ({
-      ...prev,
-      idade: prev.idade + 1,
-    }));
-  }
+//   function fazerAniversario() {
+//     setUsuario((prev) => ({
+//       ...prev,
+//       idade: prev.idade + 1,
+//     }));
+//   }
 
-  return (
-    <div>
-      <p>
-        {usuario.nome} tem {usuario.idade} anos
-      </p>
-      <button onClick={fazerAniversario}>Fazer aniversário</button>
-    </div>
-  );
+//   return (
+//     <div>
+//       <p>
+//         {usuario.nome} tem {usuario.idade} anos
+//       </p>
+//       <button onClick={fazerAniversario}>Fazer aniversário</button>
+//     </div>
+//   );
 }
